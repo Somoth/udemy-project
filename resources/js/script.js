a88af90540ef3d69213064e41dc544a516b9c16c
Svitlana Sytnyk <sytnyksy@gmail.com>
2019-03-12 11:59:23 +0200

    Fixed bottom

--- resources/js/script.js
+++ resources/js/script.js
@@ -60,12 +60,12 @@ $(document).ready(function() {
         var nav = $(".js--main-nav");
         var icon = $(".js--nav-icon i");
         nav.slideToggle(200);
-        if (icon.hasClass("ion-navicon-round")) {
-            icon.addClass("ion-close-round");
-            icon.removeClass("ion-navicon-round");
-        } else {
+        if (icon.hasClass("ion-close-round")) {
             icon.addClass("ion-navicon-round");
             icon.removeClass("ion-close-round");
+        } else {
+            icon.addClass("ion-closen-round");
+            icon.removeClass("ion-navicon-round");
         }
         
         
